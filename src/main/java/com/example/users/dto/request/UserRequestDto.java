4b58fa3aee107e48e7bcc3eacdc9d48ecc8c71b7
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-10-06 21:32:30 +0300

    commit

--- src/main/java/com/example/users/dto/request/UserRequestDto.java
+++ src/main/java/com/example/users/dto/request/UserRequestDto.java
@@ -1,9 +1,13 @@
 package com.example.users.dto.request;
 
+import com.example.users.validator.ValidAge;
 import com.example.users.validator.ValidEmail;
+import lombok.Data;
+
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@Data
 public class UserRequestDto {
     @NotNull
     @ValidEmail
@@ -13,6 +17,7 @@ public class UserRequestDto {
     @NotNull
     private String lastName;
     @NotNull
+    @ValidAge
     private LocalDate birthDate;
     private String address;
     private String phone;
